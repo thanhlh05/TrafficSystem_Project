41b28a53d2e3cf2a535cb4993f0ac96d512fc77d
GH2909 <nguyengiangjm@gmail.com>
2026-06-29 23:26:35 +0700

    SignalR done

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69c8263d416c3993f026ee6d3e6d12398ef2591b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+705cc9d79d98931188cf13b20e41ed706f20995a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
